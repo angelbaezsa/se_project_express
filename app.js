906db13606ed62932ab0173d367a4894e0c2399d
Angel Baez <angelbaezsa@gmail.com>
2024-02-19 21:29:03 -0500

    implemented winston pckg

--- app.js
+++ app.js
@@ -12,16 +12,21 @@ app.use(cors());
 const { PORT = 3001 } = process.env;
 const errorHandler = require("./middlewares/error-handler"); // centralized error handler
 
-app.listen(PORT, () => console.log(`app listening in port: ${PORT}`));
-
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", (r) =>
   console.log(`DB error ${r}`),
 );
+
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+
+app.use(requestLogger);
 const routes = require("./routes");
 
 app.use(express.json());
 app.use(cors());
 
 app.use(routes);
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`app listening in port: ${PORT}`));
