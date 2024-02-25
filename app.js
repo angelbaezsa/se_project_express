2f8b75d149924ec7ed1213479272615e5179d0ac
Angel Baez <angelbaezsa@gmail.com>
2024-02-24 21:53:28 -0500

    Added crash test piece of code

--- app.js
+++ app.js
@@ -24,6 +24,12 @@ const routes = require("./routes");
 app.use(express.json());
 app.use(cors());
 
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 app.use(routes);
 app.use(errorLogger);
 app.use(errors());
