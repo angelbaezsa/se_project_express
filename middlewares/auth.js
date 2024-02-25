09b127949e36bb0ac1d8a5b2efdd2ac3d0188916
Angel Baez <angelbaezsa@gmail.com>
2024-02-25 13:34:50 -0500

    auth.js secret key now changed to a more dynamic approach, will choose between the secret or jwt_secret

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const UnauthorizedError = require("../errorConstructors/UnauthorizedError");
 // const { UNAUTHORIZED } = require("../utils/errors");
 
-const { JWT_SECRET } = process.env;
+const { JWT_SECRET, NODE_ENV } = process.env;
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
@@ -15,9 +15,12 @@ const auth = (req, res, next) => {
   const token = authorization.replace("Bearer ", "");
   let payload;
 
+  const secretKey = NODE_ENV === "production" ? JWT_SECRET : "dev-secret";
+
   try {
     // switched from "dev-secret" to JWT_secret
-    payload = jwt.verify(token, JWT_SECRET);
+    // payload now will use jwt or "dev-secret" depending on the environment"
+    payload = jwt.verify(token, secretKey);
   } catch (err) {
     // res.status(UNAUTHORIZED.error).send({ message: UNAUTHORIZED.status });
     next(new UnauthorizedError("You are not authorized"));
